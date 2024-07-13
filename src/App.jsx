573fefb8e7fb44e162dd0caf9cec4697a84f5c0c
Alex <geor0247@algonquinlive.com>
2024-07-13 04:31:18 -0400

    Completes the assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Notifications from './notifications.js';
+
+
+function NotificationMessage({ notification, onClear }) {
+  return (
+    <li key={notification.id}>
+      <strong>{notification.name}:</strong> {notification.message}
+      <button className="btn btn-primary" onClick={onClear}>Clear</button>
+    </li>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [notifications, setNotifications] = useState(Notifications);
+
+  const clearNotification = (id) => {
+    setNotifications(notifications.filter(notification => notification.id !== id));
+  };
+
+  const clearAllNotifications = () => {
+    setNotifications([]);
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Notifications ({notifications.length})</h1>
+      <button class="btn btn-link" onClick={clearAllNotifications}>Clear All</button>
+      <ul>
+        {notifications.map(notification => (
+          <NotificationMessage
+            key={notification.id}
+            notification={notification}
+            onClear={() => clearNotification(notification.id)}
+          />
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App
